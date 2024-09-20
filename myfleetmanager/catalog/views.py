4eea75c10a2806974aa2e9d6b7b6911999b14435
BHaessler <e.a.haessler@gmail.com>
2024-09-20 13:39:51 -0400

    added html and css to begin the process of developing the front end

--- myfleetmanager/catalog/views.py
+++ myfleetmanager/catalog/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
-
+# Everything under here I added
+from .models import Owner, VehicleType, CarMake, CarInstance
 # Create your views here.
+
+def index(request):
+    """View function for the homepage of the site"""
+
+    # Generate counts of some of the main objects
+    num_cars = CarMake.objects.all().count()
+    num_instances = CarInstance.objects.all().count()
+
+    num_owners = Owner.objects.all().count()
+
+
+    context = {
+        'num_cars': num_cars,
+        'num_instances': num_instances,
+
+        'num_owners': num_owners,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
